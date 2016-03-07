6585c50b746740b4ddaa6b96af414b44431c4fa7
Matan Gold <matan@quickode.com>
2016-03-07 16:35:27 +0200

    * VioozerSDK for SA - Version: 1.3.21

---
+++ VioozerSDK-lib/VioozerSDK.h
@@ -0,0 +1,18 @@
+//
+//  VioozerSDK.h
+//  VioozerSDK
+//
+//  Created by Zeev on 12/1/14.
+//  Copyright (c) 2014 Quickode. All rights reserved.
+//
+// VioozerSDK version 1.3.21
+#define VIOOZER_SDK_VERSION @"1.3.21"
+
+#import <Foundation/Foundation.h>
+#import "VioozerLocation.h"
+#import "VioozerUser.h"
+#import "VioozerShare.h"
+#import "VioozerQuestion.h"
+#import "VioozerFeedItem.h"
+#import "VioozerAPI.h"
+
