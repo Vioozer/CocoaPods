3d50acad368253a0a67b1a9fafefbf6238f0568d
dobrogit <paolo.dobro@gmail.com>
2018-03-20 10:56:07 -0700

    Push 2.0.6 version with iPhoneX support

---
+++ Podfile
@@ -0,0 +1,5 @@
+# Provided by Balcony http://www.balcony.io/ 
+
+target 'MY-APP-NAME' do
+    pod 'Vioozer', :git => 'https://github.com/Vioozer/CocoaPods.git'
+end
---
+++ VioozerSDK-lib/VioozerSDK.h
@@ -0,0 +1,16 @@
+//
+//  VioozerSDK.h
+//  VioozerSDK
+//
+//  Created by Zeev on 12/1/14.
+//  Copyright (c) 2014 Quickode. All rights reserved.
+//
+#define VIOOZER_SDK_VERSION @"2.0.5"
+
+#import <Foundation/Foundation.h>
+#import "VioozerLocation.h"
+#import "VioozerUser.h"
+#import "VioozerShare.h"
+#import "VioozerQuestion.h"
+#import "VioozerFeedItem.h"
+#import "VioozerAPI.h"
